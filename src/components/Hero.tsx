2d2cb9a36e0ee6526141995da831ff55eb2c50f7
Joshua Smith <61332947+joshuatsmith96@users.noreply.github.com>
2023-10-18 11:54:20 -0500

    Initial Commit
    Created a new react app using 'vite'.

---
+++ src/components/Hero.tsx
@@ -0,0 +1,19 @@
+
+
+const aloop = () => {
+    return(
+        <h1>Nice</h1>
+    )
+}
+
+
+function Hero() {
+
+  return (
+    <div className="Hero">
+      {aloop()}
+    </div>
+  )
+}
+
+export default Hero;

bc1a47617f92879838c588766079338cfc4d01ca
Joshua Smith <61332947+joshuatsmith96@users.noreply.github.com>
2023-10-19 10:01:50 -0500

    Hero Section Animation Updates
    Adding additional functionality to Hero Section.

--- src/components/HeaderBar.tsx
+++ src/components/HeaderBar.tsx
@@ -1,10 +1,9 @@
 import ToolTip from './ToolTip'
-import GitHubImage from '../assets/Images/GitHub.png'
 
 function HeaderBar(){
     return(
         <div className="HeaderBar">
-            <h3>Joshua</h3>
+            <h3>Portfolio</h3>
             <ToolTip Msg={"Visit Joshua's "} Bold={"GitHub"}/>
         </div>
     )
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -2,13 +2,44 @@ function Hero() {
   let lastKnownScrollPosition = 0;
   let ticking = false;
 
-  function doSomething(scrollPos:any) {
-    //Get Name Element
+  function doSomething(scrollPos: any) {
+    //------------------- NAME SCROLL AFFECTS --------------------------------
     let name = document.getElementById("gradient-name");
-    let size = 90 + (scrollPos / 2);
-    let letterSpacing = 0 + (scrollPos / 2);
-    let blurvalue = 0 + (scrollPos / 60)
-    name?.setAttribute("style", "font-size: " + size + "px;" + " letter-spacing: " + letterSpacing + "px;")
+    let size = 90 + (scrollPos / 5);
+    let letterSpacing = 0 + scrollPos;
+
+    if (scrollPos <= 253){
+      console.log("moving!")
+      name?.setAttribute(
+        "style",
+        "font-size: " + size + "px;" + " letter-spacing: " + letterSpacing + "px;"
+      );
+      //------------------- INTRO SCROLL AFFECTS --------------------------------
+      let intro = document.getElementById("hero-intro")
+      let intro_pos = 100 - (scrollPos*2) + "px"
+      intro?.setAttribute(
+        "style",
+        "top: " + intro_pos
+      )
+  
+      //------------------- HERO BACKGROUND SCROLL AFFECTS --------------------------------
+      let hero_background = document.getElementById("hero");
+      hero_background?.setAttribute(
+        "style",
+        "background-image: linear-gradient(#6363f1 " +
+          scrollPos / 15 +
+          "%, #5252ce " +
+          scrollPos / 10 +
+          "%, #4242a7 " +
+          scrollPos / 5 +
+          "%, #37378b " +
+          scrollPos +
+          "%, #2a2a6d 100%)"
+      );
+    } else {
+      console.log("not moving")
+    }
+
   }
 
   document.addEventListener("scroll", () => {
@@ -25,8 +56,11 @@ function Hero() {
   });
 
   return (
-    <div className="Hero">
-      <h1 className="gradient-text" id="gradient-name">Joshua</h1>
+    <div className="Hero" id="hero">
+      <p id="hero-intro">Hello, I'm</p>
+      <h1 className="gradient-text" id="gradient-name">
+        Joshua
+      </h1>
     </div>
   );
 }

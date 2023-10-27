d7f7513d49a58efbfdec83fc0fb0e1f4503c4a63
Joshua Smith <61332947+joshuatsmith96@users.noreply.github.com>
2023-10-27 14:58:28 -0500

    Link Update
    Updating the "Tic Tac Toe" link in the "Projects" section.

--- src/components/Project/ProjectSection.tsx
+++ src/components/Project/ProjectSection.tsx
@@ -10,7 +10,7 @@ function ProjectSection(){
         <div className="ProjectSection">
             <h1 className="text-white">Projects</h1>
             <ProjectCard id="1" Title={"Quiz-It (React, CSS, Typescript)"} Text={"Test your knowledge on various topics to include Starwars and Fruit! Not familiar with the topic? Try practice mode!"} viewGithubLink="https://github.com/joshuatsmith96/Quiz-It" viewSiteLink="https://joshuatsmith96.github.io/Quiz-It/" mainImage={QuizItThumbnail}/>
-            <ProjectCard id="2" Title={"Tic Tac Toe (React, CSS, Typescript)"} Text={"Play a simple game of tic tac toe! Choose from a small assortment of unique game pieces and have some fun!"} viewGithubLink="https://github.com/joshuatsmith96/Quiz-It" viewSiteLink="https://joshuatsmith96.github.io/Quiz-It/" mainImage={TicTacToeThumbnail}/>
+            <ProjectCard id="2" Title={"Tic Tac Toe (React, CSS, Typescript)"} Text={"Play a simple game of tic tac toe! Choose from a small assortment of unique game pieces and have some fun!"} viewGithubLink="https://github.com/joshuatsmith96/Tic-Tac-Toe-V2" viewSiteLink="https://joshuatsmith96.github.io/Tic-Tac-Toe-V2/" mainImage={TicTacToeThumbnail}/>
             <ProjectCard id="3" Title={"Expense Calculator (HTML, CSS, JavaScript)"} Text={"A calculator that shows you how much your spending on expenses."} viewGithubLink="https://github.com/joshuatsmith96/budgetplanner" viewSiteLink="https://joshuatsmith96.github.io/budgetplanner/" mainImage={MoneyBag}/>
             <ProjectCard id="4" Title={"Portfolio Website (React, CSS, Typescript)"} Text={"View the code for this website!"} viewGithubLink="https://github.com/joshuatsmith96/budgetplanner" viewSiteLink="n" mainImage={PortfolioThumbnail}/>
         </div>

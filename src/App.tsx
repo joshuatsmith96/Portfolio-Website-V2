85de36ea5b48a48a42290f70f1e41ec339f4d858
Joshua Smith <61332947+joshuatsmith96@users.noreply.github.com>
2023-10-27 13:08:19 -0500

    Removed Router
    Removed Router since its not being used

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,12 @@
 import "./App.css";
 import Home from "./Home";
 import HeaderBar from './components/HeaderBar';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <HeaderBar />
-      <BrowserRouter>
-        <Routes>
-          <Route index element={<Home />} />
-        </Routes>
-      </BrowserRouter>
+      <Home />
     </div>
   );
 }
--- src/components/Project/ProjectSection.tsx
+++ src/components/Project/ProjectSection.tsx
@@ -2,6 +2,7 @@ import "./ProjectStyles.css"
 import ProjectCard from "./ProjectCard"
 import QuizItThumbnail from '../../assets/Images/QuizItThumbnail.png'
 import TicTacToeThumbnail from '../../assets/Images/TicTacToeThumbnail.png'
+import MoneyBag from '../../assets/Images/moneybag.png'
 
 function ProjectSection(){
     return(
@@ -9,7 +10,7 @@ function ProjectSection(){
             <h1 className="text-white">Projects</h1>
             <ProjectCard Title={"Quiz-It (React, CSS, Typescript)"} Text={"Test your knowledge on various topics to include Starwars and Fruit! Not familiar with the topic? Try practice mode!"} viewGithubLink="https://github.com/joshuatsmith96/Quiz-It" viewSiteLink="https://joshuatsmith96.github.io/Quiz-It/" mainImage={QuizItThumbnail}/>
             <ProjectCard Title={"Tic Tac Toe (React, CSS, Typescript)"} Text={"Play a simple game of tic tac toe! Choose from a small assortment of unique game pieces and have some fun!"} viewGithubLink="https://github.com/joshuatsmith96/Quiz-It" viewSiteLink="https://joshuatsmith96.github.io/Quiz-It/" mainImage={TicTacToeThumbnail}/>
-            <ProjectCard Title={"Expense Calculator (HTML, CSS, JavaScript)"} Text={""} viewGithubLink="https://github.com/joshuatsmith96/budgetplanner" viewSiteLink="https://joshuatsmith96.github.io/budgetplanner/" mainImage={TicTacToeThumbnail}/>
+            <ProjectCard Title={"Expense Calculator (HTML, CSS, JavaScript)"} Text={"A calculator that shows you how much your spending on expenses."} viewGithubLink="https://github.com/joshuatsmith96/budgetplanner" viewSiteLink="https://joshuatsmith96.github.io/budgetplanner/" mainImage={MoneyBag}/>
         </div>
     )
 }

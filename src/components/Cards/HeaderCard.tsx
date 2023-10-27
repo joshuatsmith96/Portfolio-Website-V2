9ea0170981ea33fe004d00149280ad7db958f30c
Joshua Smith <61332947+joshuatsmith96@users.noreply.github.com>
2023-10-27 14:47:46 -0500

    "Who Am I" update
    Updated some text for the "Who Am I" section.

--- src/components/Cards/HeaderCard.tsx
+++ src/components/Cards/HeaderCard.tsx
@@ -5,7 +5,7 @@ function HeaderCard(){
             <div className="ProfileImageContainer">
             <div className="ProfileImage" />
             </div>
-            <p>My name is Joshua Smith. I am a Web Specialist, and a US Army veteran. I was a student at the Anoka Technical College in Anoka, MN from 2019 to 2021.<br /><br/> I am currently a variable data programmer and website manager for a company called Daily Printing in Plymouth, MN. My favorite hobby is to fly drones and record videos!</p>
+            <p>My name is Joshua Smith. I am a Web Developer, and a US Army veteran. I was a student at the Anoka Technical College in Anoka, MN from 2019 to 2021.<br /><br/> I am currently a variable data programmer and website manager for a company called Daily Printing in Plymouth, MN. My favorite hobby is to fly drones and record videos!</p>
         </div>
     )
 }

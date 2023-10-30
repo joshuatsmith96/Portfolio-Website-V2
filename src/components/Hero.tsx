e25ef6ac8ff29ab37f89bf3c910623910b81df4f
Joshua Smith <61332947+joshuatsmith96@users.noreply.github.com>
2023-10-30 10:56:07 -0500

    Hero Change
    Added some new animations to the hero section

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -2,6 +2,10 @@ function Hero() {
 
   return (
     <div className="Hero" id="hero">
+      <div id="square1" className="square"></div>
+      <div id="square2" className="square"></div>
+      <div id="square3" className="square"></div>
+      <div id="square4" className="square"></div>
       <p id="hero-intro">Hello, I'm</p>
       <h1 className="gradient-text" id="gradient-name">
         Joshua
--- src/components/ScrollAnimations.tsx
+++ src/components/ScrollAnimations.tsx
@@ -1,9 +1,8 @@
 let lastKnownScrollPosition = 0;
 let ticking = false;
-//Get all icons up here and put them into a 
+//Get all icons up here and put them into a
 
 function ScrollAnimations() {
-  
   function doSomething(scrollPos: number) {
     //------------------- NAME SCROLL AFFECTS --------------------------------
     let name = document.getElementById("gradient-name");
@@ -11,7 +10,7 @@ function ScrollAnimations() {
     let size = 90 + scrollPos / 5;
     let letterSpacing = 0 + scrollPos;
 
-    if (scrollPos <= 180) {
+    if (scrollPos <= 300) {
       name?.setAttribute(
         "style",
         "font-size: " +
@@ -22,31 +21,50 @@ function ScrollAnimations() {
           "px;"
       );
 
-      headerbar?.setAttribute(
-        "style",
-        "opacity: " + (1 - (scrollPos / 800))
-      )
+      headerbar?.setAttribute("style", "opacity: " + (1 - scrollPos / 800));
       //------------------- INTRO SCROLL AFFECTS --------------------------------
       let intro = document.getElementById("hero-intro");
       let intro_pos = 100 - scrollPos * 2 + "px";
       intro?.setAttribute("style", "top: " + intro_pos);
 
       //------------------- HERO BACKGROUND SCROLL AFFECTS --------------------------------
-      let hero_background = document.getElementById("hero");
-      hero_background?.setAttribute(
-        "style",
-        "background-image: linear-gradient(#6363f1 " +
-          scrollPos / 3 +
-          "%, #5252ce " +
-          scrollPos / 10 +
-          "%, #4242a7 " +
-          scrollPos / 5 +
-          "%, #37378b " +
-          scrollPos +
-          "%, #2a2a6d 100%)"
-      );
+      // let square1 = document.getElementById("square1")
+      // let square2 = document.getElementById("square2")
+      // let square3 = document.getElementById("square3")
+      // let opacity = 0 + (scrollPos / 50)
+      // let bottom_positioning1 = 20 - (scrollPos / 6)
+      // let left_positioning1 = 10 - (scrollPos / 6)
+      // let bottom_positioning2 = 40 - (scrollPos / 4)
+      // let left_positioning2 = 20 - (scrollPos / 4)
+      // let bottom_positioning3 = 60 - (scrollPos / 2)
+      // let left_positioning3 = 40 - (scrollPos / 2)
+      // square1?.setAttribute("style", "bottom: " + bottom_positioning1 + "%; left: " + left_positioning1 + "%; opacity: " + opacity + ";")
+      // square2?.setAttribute("style", "bottom: " + bottom_positioning2 + "%; left: " + left_positioning2 + "%; opacity: " + opacity + ";" )
+      // square3?.setAttribute("style", "bottom: " + bottom_positioning3 + "%; left: " + left_positioning3 + "%; opacity: " + opacity + ";")
+      //ScrollPos, Square, bottom pos, left pos, speed
+      if (window.innerWidth < 960) {
+        moveSquare(scrollPos, "1", 20, 10, 2);
+        moveSquare(scrollPos, "2", 23, 10, 2.5);
+        moveSquare(scrollPos, "3", 26, 10, 3);
+        moveSquare(scrollPos, "4", 20, 10, 3.5);
+      } else {
+        moveSquare(scrollPos, "1", 0, 85, 5);
+        moveSquare(scrollPos, "2", 0, 82, 4.5);
+        moveSquare(scrollPos, "3", 0, 79, 4);
+        moveSquare(scrollPos, "4", 0, 88, 3.5);
+      }
+      
     }
   }
+
+  let moveSquare = (scrollPos: number, squarenum: string, bottom_pos: number, left_pos: number, speed: number) => {
+    let square = document.getElementById("square" + squarenum);
+    let bottom_positioning = bottom_pos - scrollPos / speed;
+    let left_positioning = left_pos - scrollPos / speed;
+    let opacity = 0 + (scrollPos / 50);
+
+    square?.setAttribute("style", "bottom: " + bottom_positioning + "%; left: " + left_positioning + "%; opacity: " + opacity + ";")
+  };
 
   document.addEventListener("scroll", () => {
     lastKnownScrollPosition = window.scrollY;
